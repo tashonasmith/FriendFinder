8644a089f63f8f018eec73b081d4888f1c8efd51
Tashona Smith <tashona.smith@gmail.com>
2020-05-29 22:05:19 -0500

    Create friendsArray JSON object

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+
+var friendsArray = [
+    {
+        
+        name: "Joe Exotic",
+        photo: "https://www.businessinsider.in/photo/75542979/One-murder-for-hire-plot-5-husbands-and-176-tigers-Meet-Joe-Exotic-the-man-Nicolas-Cage-will-play-in-an-upcoming-TV-series.jpg?imgsize=395590",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+          
+    },
+    {
+        name: "Ryan Reynolds",
+        photo: "https://news.harvard.edu/wp-content/uploads/2017/01/ryan-reynolds-headshot_horizontal_605.jpg?w=605&h=403&crop=1",
+        scores: [
+            5,
+            3,
+            4,
+            1,
+            2,
+            2,
+            5,
+            1,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Jason Momoa",
+        photo: "https://www.syfy.com/sites/syfy/files/styles/1200x680_hero/public/2019/04/jason-momoa-aquaman.jpg",
+        scores: [
+            3,
+            1,
+            4,
+            4,
+            2,
+            5,
+            1,
+            3,
+            4,
+            2
+        ]  
+    },
+    {
+        name: "Scarlett Johansson",
+        photo: "https://www.lovethispic.com/uploaded_images/351882-Scarlett-Johansson.jpg",
+        scores: [
+            1,
+            4,
+            3,
+            5,
+            5,
+            2,
+            1,
+            3,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Kourtney Kardashian",
+        photo: "https://pmchollywoodlife.files.wordpress.com/2016/01/kourtney-kardashian-shiny-hair-ftr.jpg",
+        scores: [
+            5,
+            4,
+            4,
+            2,
+            1,
+            3,
+            5,
+            1,
+            2,
+            4
+        ]
+    }
+]
